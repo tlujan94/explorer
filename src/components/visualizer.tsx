dc1587748497cdaab29861f50c6c79d7a30eb7b1
Thomas Lujan <tlujan94@rams.colostate.edu>
2021-03-15 13:49:12 -0600

    Setup leaflet

--- src/components/visualizer.tsx
+++ src/components/visualizer.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
+import L from 'leaflet';
 
 export class Visualizer extends React.Component<{}, {}> {
+    explorer: L.Map;
+
     render() {
-        return <div>testing...</div>
+        return <div id='explorer' style={{height: '512px', width: '512px', float: 'left'}}/>
+    }
+
+    componentDidMount(): void {
+        this.explorer = L.map('explorer', {
+           center: [37.8, -96],
+           zoom: 3
+        });
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.explorer);
     }
 }
